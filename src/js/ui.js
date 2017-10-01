814b83d88d5f2451f7511d0ba7a9e65fbe0b722b
Lortex <lucas.pluvinage@gmail.com>
2017-10-02 00:58:48 +0200

    Better syntax for the UI class

--- src/js/ui.js
+++ src/js/ui.js
@@ -6,7 +6,6 @@ window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequest
  */
 
 function UIController() {
-    var self = this;
     this.mouseMoving = false;
     this.lastPosition = null;
 
@@ -15,49 +14,47 @@ function UIController() {
     this.project.layerList[0].reset();
     this.viewport.setLayerList(this.project.layerList);
     this.viewport.viewportDimensionsChanged();
-
-    this.onToolboxClicked = function() {
-        if (this.innerHTML === "brush") {
-            self.project.changeTool(getToolByName("TestTool"));
-        } else {
-            console.warn("Unimplemented tool.");
-        }
-    };
-
-    this.onMouseDown = function(event) {
-        self.lastPosition = self.viewport.globalToLocalPosition(new Vec2(event.offsetX, event.offsetY));
-        self.mouseMoving = true;
-
-        self.project.mouseClick(self.lastPosition);
-        window.requestAnimationFrame(self.step);
-    };
-
-    this.onMouseUp = function(event) {
-        self.lastPosition = self.viewport.globalToLocalPosition(new Vec2(event.offsetX, event.offsetY));
-        self.mouseMoving = false;
-        self.project.mouseRelease(self.lastPosition);
-    };
-
-    this.onMouseMove = function(event) {
-        self.lastPosition = self.viewport.globalToLocalPosition(new Vec2(event.offsetX, event.offsetY));
-    };
-
-
-    this.step = function(timestamp) {
-        if(self.project.mouseMove(self.lastPosition)) {
-            self.viewport.renderLayers();
-            if (self.mouseMoving) {
-                window.requestAnimationFrame(self.step);
-            }
-        }
-    }
-
-    this.windowResized = function (newSize) {
-        self.viewport.viewportDimensionsChanged();
-    }
-
-
 }
+
+UIController.prototype.onToolboxClicked = function(event) {
+    if (event.target.innerHTML === "brush") {
+        this.project.changeTool(getToolByName("TestTool"));
+    } else {
+        console.warn("Unimplemented tool.");
+    }
+};
+
+UIController.prototype.onMouseDown = function(event) {
+    this.lastPosition = this.viewport.globalToLocalPosition(new Vec2(event.offsetX, event.offsetY));
+    this.mouseMoving = true;
+
+    this.project.mouseClick(this.lastPosition);
+    window.requestAnimationFrame(this.step.bind(this));
+};
+
+UIController.prototype.onMouseUp = function(event) {
+    this.lastPosition = this.viewport.globalToLocalPosition(new Vec2(event.offsetX, event.offsetY));
+    this.mouseMoving = false;
+    this.project.mouseRelease(this.lastPosition);
+};
+
+UIController.prototype.onMouseMove = function(event) {
+    this.lastPosition = this.viewport.globalToLocalPosition(new Vec2(event.offsetX, event.offsetY));
+};
+
+
+UIController.prototype.step = function(timestamp) {
+    if(this.project.mouseMove(this.lastPosition)) {
+        this.viewport.renderLayers();
+        if (this.mouseMoving) {
+            window.requestAnimationFrame(this.step.bind(this));
+        }
+    }
+};
+
+UIController.prototype.windowResized = function (newSize) {
+    this.viewport.viewportDimensionsChanged();
+};
 
 
 var controller;
@@ -65,13 +62,12 @@ var controller;
 $(document).ready(function() {
     controller = new UIController();
 
-    $("#toolbox-container").children().click(controller.onToolboxClicked);
-    $("#viewport").mousedown(controller.onMouseDown);
-    $("#viewport").mouseup(controller.onMouseUp);
-    $("#viewport").mousemove(controller.onMouseMove);
+    $("#toolbox-container").children().click(controller.onToolboxClicked.bind(controller));
+    $("#viewport").mousedown(controller.onMouseDown.bind(controller));
+    $("#viewport").mouseup(controller.onMouseUp.bind(controller));
+    $("#viewport").mousemove(controller.onMouseMove.bind(controller));
 
-    $(window).on('resize', function(e) {
-        console.log("Window resized: " + $(this).width() + "x" + $(this).height());
-        controller.windowResized(Vec2($(this).width(), $(this).height()));
-    });
+    $(window).on('resize', (function(e) {
+        controller.windowResized(Vec2($(window).width(), $(window).height()));
+    }).bind(controller));
 });
