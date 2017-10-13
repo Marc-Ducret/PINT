21b860dc012833985e14ad0df55b4cb1fe7c2f25
Marc <vengeurk@gmailc.om>
2017-10-13 15:10:50 +0200

    selection border unit test

---
+++ test/test-selection.js
@@ -0,0 +1,38 @@
+describe('Selection border', function () {
+
+    beforeEach(function () {
+        jasmine.addMatchers({
+            isDisjoint: function () {
+                return {
+                    compare: function (actual, expected) {
+                        var result = true;
+                        for (var i = 0; i < actual.length; i++) {
+                            if(actual[i] > 0 && expected > 0) {
+                                result = false;
+                            }
+                        }
+                        return {
+                            pass: result
+                        };
+                    }
+                }
+            }
+        });
+    });
+
+    it('is disjoint with the selection', function() {
+        for (var i = 0; i < 500; i++) {
+            var selection = [];
+            var w = 800;
+            var h = 600;
+            for (var i = 0; i < w*h; i++) {
+                if (Math.random() > .95) {
+                    selection.push(1);
+                } else {
+                    selection.push(0);
+                }
+            }
+            expect(border(selection)).isDisjoint(selection);
+        }
+    });
+});
