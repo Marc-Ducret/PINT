29ab2025b99a68b138fd1d0034587c5fc5df060b
Marc <vengeurk@gmailc.om>
2017-10-26 09:51:26 +0200

    circle selection

--- src/ts/tools/selectionTool.ts
+++ src/ts/tools/selectionTool.ts
@@ -52,6 +52,19 @@ export class SelectionTool extends Tool {
                 }
                 break;
             case "circle":
+                let ct = 0;
+                let radius = Math.ceil(this.firstCorner.distance(this.lastCorner));
+                console.log("counting within ", radius);
+                for (let y = this.firstCorner.y - radius; y < this.firstCorner.y + radius; y++) {
+                    for (let x = this.firstCorner.x - radius; x < this.firstCorner.x + radius; x++) {
+                        if(x >= 0 && x < this.project.dimensions.x && y >= 0 && y < this.project.dimensions.y
+                            && (x - this.firstCorner.x) ** 2 + (y - this.firstCorner.y) ** 2 <= radius ** 2) {
+                            selection[x + y * this.project.dimensions.x] = 0xFF;
+                            ct++;
+                        }
+                    }
+                }
+                console.log("found ", ct);
                 break;
             case "ellipse":
                 break;
