fd468e82d6d1d0968760d354a242258b716700ad
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2017-10-29 14:00:12 +0100

    fixed some variable declarations

--- test/test-selection.ts
+++ test/test-selection.ts
@@ -20,8 +20,8 @@ describe('Selection border', function () {
             isDisjoint: function () {
                 return {
                     compare: function (actual, expected) {
-                        var result = true;
-                        for (var i = 0; i < actual.length; i++) {
+                        let result = true;
+                        for (let i = 0; i < actual.length; i++) {
                             if(actual[i] > 0 && expected[i] > 0) {
                                 result = false;
                             }
@@ -36,11 +36,11 @@ describe('Selection border', function () {
     });
 
     it('is disjoint with the selection', function() {
-        for (var i = 0; i < 50; i++) {
-            var w = 800;
-            var h = 600;
-            var selection = new Uint8ClampedArray(w*h);
-            for (var i = 0; i < w*h; i++) {
+        for (let i = 0; i < 50; i++) {
+            const w = 800;
+            const h = 600;
+            let selection = new Uint8ClampedArray(w*h);
+            for (let i = 0; i < w*h; i++) {
                 if (Math.random() > .95) {
                     selection[i] = 0xFF;
                 }
@@ -56,9 +56,9 @@ describe('Shape selection', function () {
             isConvex: function () {
                 return {
                     compare: function (actual, expected) {
-                        var result = true;
-                        var incr = true;
-                        for (var i = 1; i < actual.length; i++) {
+                        let result = true;
+                        let incr = true;
+                        for (let i = 1; i < actual.length; i++) {
                             if(actual[i] < actual[i-1] && incr) {
                                 incr = false;
                             } else if(actual[i] > actual[i-1] && !incr) {
@@ -76,11 +76,11 @@ describe('Shape selection', function () {
     });
 
     it('is convex', function() {
-        var tool = new SelectionTool();
-        var proj = new Project('test project');
-        for (var i = 0; i < 5; i++) {
-            var w = 50;
-            var h = 50;
+        let tool = new SelectionTool();
+        let proj = new Project('test project');
+        for (let i = 0; i < 5; i++) {
+            const w = 50;
+            const h = 50;
 
             let randPos = function() {
                 return new Vec2(Math.floor(Math.random() * w), Math.floor(Math.random() * h));
@@ -93,19 +93,19 @@ describe('Shape selection', function () {
             tool.startUse(null, randPos(), proj);
             tool.endUse(randPos());
 
-            var row = new Uint8ClampedArray(w);
-            for(var y = 0; y < h; y++) {
+            let row = new Uint8ClampedArray(w);
+            for(let y = 0; y < h; y++) {
                 if(Math.random() < 1) {
-                    for(var x = 0; x < w; x++) {
+                    for(let x = 0; x < w; x++) {
                         row[x] = proj.currentSelection.values[x + y*proj.dimensions.x];
                     }
                     expect(row).isConvex(true);
                 }
             }
-            var col = new Uint8ClampedArray(h);
-            for(var x = 0; x < w; x++) {
+            let col = new Uint8ClampedArray(h);
+            for(let x = 0; x < w; x++) {
                 if(Math.random() < 1) {
-                    for(var y = 0; y < h; y++) {
+                    for(let y = 0; y < h; y++) {
                         col[y] = proj.currentSelection.values[x + y*proj.dimensions.x];
                     }
                     expect(col).isConvex(true);
