9985f26ad3665097cca1a70a9fbd9ba6fac0a743
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2017-12-17 22:59:59 +0100

    Added serialization of selection

--- src/client/ts/selection/selection.ts
+++ src/client/ts/selection/selection.ts
@@ -8,11 +8,34 @@ import {drawSelection, resetBorder} from "./selectionRender";
 import {computeBorder} from "./selectionUtils";
 import {Layer} from "../ui/layer";
 
+export interface SerializedPixelSelectionHandler {
+    dataUrl: string;
+    width: number;
+    height: number;
+}
+
+export function PixelSelectionHandlerFromSerialized (serialized: SerializedPixelSelectionHandler): PixelSelectionHandler {
+    let w = serialized.width;
+    let h = serialized.height;
+    let obj = new PixelSelectionHandler(null, w, h);
+
+
+    let img = new Image;
+    img.onload = function(){
+        obj.getMask().getContext('2d').drawImage(img, 0, 0);
+        let imgdata = obj.getMask().getContext('2d').getImageData(0, 0, w, h);
+        obj.values.forEach((val, i) => {
+            obj.values[i] = imgdata[4*i];
+        });
+    }.bind(this);
+    img.src = serialized.dataUrl;
+    return obj;
+}
 
 export class PixelSelectionHandler {
     private width: number;
     private height: number;
-    private values: Uint8ClampedArray;
+    public values: Uint8ClampedArray;
     private border: Array<Vec2>;
 
     private mask: Layer;
@@ -20,27 +43,36 @@ export class PixelSelectionHandler {
     private animation_image_data: ImageData;
     private animation_context: CanvasRenderingContext2D;
 
-    constructor(context: CanvasRenderingContext2D, w: number, h:number) {
+    serialize(): SerializedPixelSelectionHandler {
+        return {dataUrl: this.mask.getHTMLElement().toDataURL(),
+                width: this.width,
+                height: this.height};
+    }
+
+
+    constructor(context: CanvasRenderingContext2D, w: number, h: number) {
         this.width = w;
         this.height = h;
-        this.values = new Uint8ClampedArray(w*h);
+        this.values = new Uint8ClampedArray(w * h);
         this.border = [];
 
-        this.mask = new Layer(new Vec2(w,h));
+        this.mask = new Layer(new Vec2(w, h));
         this.mask.reset();
         this.mask.fill();
 
-        this.animation_context = context;
-        this.animation_image_data = context.getImageData(0, 0, w, h);
+        if (context != null) {
+            this.animation_context = context;
+            this.animation_image_data = context.getImageData(0, 0, w, h);
+        }
 
 
         // select all at the beginning.
-        for (let i=0;i<w*h;i++) {
+        for (let i = 0; i < w * h; i++) {
             this.values[i] = 0xFF;
         }
     }
 
-    getMask() : HTMLCanvasElement {
+    getMask(): HTMLCanvasElement {
         return this.mask.getHTMLElement();
     }
 
@@ -54,14 +86,14 @@ export class PixelSelectionHandler {
      *@param {Vec2} p Coordinates
      *@param {number} intensity (between 0 and 1)
      */
-    add (p: Vec2, intensity: number) {
-        let i = Math.floor(p.x) + Math.floor(p.y)*this.width;
+    add(p: Vec2, intensity: number) {
+        let i = Math.floor(p.x) + Math.floor(p.y) * this.width;
 
         let data = this.mask.getContext().getImageData(0, 0, this.width, this.height);
-	    this.values[i] = Math.min(0xFF, this.values[i] + intensity);
+        this.values[i] = Math.min(0xFF, this.values[i] + intensity);
 
-        data[4*i+3] = this.values[i];
-        data[4*i] = this.values[i];
+        data[4 * i + 3] = this.values[i];
+        data[4 * i] = this.values[i];
         this.mask.getContext().putImageData(data, 0, 0);
     }
 
@@ -70,8 +102,8 @@ export class PixelSelectionHandler {
      *@param {Vec2} p Coordinates
      *@param {number} intensity (between 0 and 1)
      */
-    retrieve (p: Vec2, intensity: number) {
-        this.values[p.x + p.y*this.width] = Math.max(0, this.values[p.x + p.y*this.width] - intensity);
+    retrieve(p: Vec2, intensity: number) {
+        this.values[p.x + p.y * this.width] = Math.max(0, this.values[p.x + p.y * this.width] - intensity);
     }
 
     /**
@@ -82,7 +114,7 @@ export class PixelSelectionHandler {
         let imagedata = this.mask.getContext().getImageData(0, 0, this.width, this.height);
         sel.forEach((val, i) => {
             this.values[i] = Math.min(0xFF, this.values[i] + val);
-            imagedata.data[4*i+3] = this.values[i];
+            imagedata.data[4 * i + 3] = this.values[i];
         });
         this.mask.getContext().putImageData(imagedata, 0, 0);
     }
@@ -91,13 +123,20 @@ export class PixelSelectionHandler {
      * Call to update the graphical representation of the selection
      */
     updateBorder() {
-        resetBorder(this.animation_image_data, this.border, this.width, this.height);
-        this.border = computeBorder(this.values, this.width, this.height);
+        if (this.animation_image_data != null) {
+            resetBorder(this.animation_image_data, this.border, this.width, this.height);
+            this.border = computeBorder(this.values, this.width, this.height);
+        }
     }
+
     /**
      * Call to draw the selection
      */
     draw() {
+        if (this.animation_context == null) {
+            console.warn("Drawing on a null context.");
+            return;
+        }
         drawSelection(this.animation_image_data, this.border, this.width, this.height);
         this.animation_context.putImageData(this.animation_image_data, 0, 0);
     }
@@ -106,9 +145,11 @@ export class PixelSelectionHandler {
      * Resets the selection to no pixel selected
      */
     reset() {
-        this.values = new Uint8ClampedArray(this.width*this.height);
+        this.values = new Uint8ClampedArray(this.width * this.height);
         this.mask.reset();
-        resetBorder(this.animation_image_data, this.border, this.width, this.height);
+        if (this.animation_image_data != null) {
+            resetBorder(this.animation_image_data, this.border, this.width, this.height);
+        }
         this.border = [];
     }
 
@@ -126,7 +167,7 @@ export class PixelSelectionHandler {
      * @return a value between 0 and 0xFF where 0 is not selected and 0xFF is fully selected
      */
     getSelectionIntensity(p: Vec2) {
-        if(p.x >= 0 && p.x < this.width && p.y >= 0 && p.y < this.height) {
+        if (p.x >= 0 && p.x < this.width && p.y >= 0 && p.y < this.height) {
             return this.values[p.x + this.width * p.y];
         } else {
             return 0;
@@ -137,3 +178,4 @@ export class PixelSelectionHandler {
         return this.border;
     }
 }
+
