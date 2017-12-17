bb067597760936ebed219a288ba7de3b3131d81b
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2017-12-17 23:02:06 +0100

    Tool action synchronization over the network

--- src/client/ts/networkLink.ts
+++ src/client/ts/networkLink.ts
@@ -1,23 +1,54 @@
 import {Project} from "./docState";
 import {ActionInterface} from "./tools/actionInterface";
+import {
+    PixelSelectionHandler, PixelSelectionHandlerFromSerialized,
+    SerializedPixelSelectionHandler
+} from "./selection/selection";
+
+export interface ActionNetworkPacket {
+    data: ActionInterface,
+    sender: string,
+}
+
+export interface HelloNetworkPacket {
+    sender: string,
+    serializedSelection: SerializedPixelSelectionHandler,
+}
 
 export class NetworkLink {
     private socket: SocketIOClient.Socket;
     private project: Project;
+    private me: string;
+
+    private selectionHandlers: {[id: string]: PixelSelectionHandler};
 
     constructor (project: Project, socket: SocketIOClient.Socket) {
         this.socket = socket;
         this.project = project;
 
+        this.me = this.socket.id;
+
+        this.selectionHandlers = {};
+        this.selectionHandlers[this.me] = this.project.currentSelection;
+
         this.socket.on("action", this.onAction.bind(this));
+        this.socket.on("hello", this.onHello.bind(this));
     }
 
     sendAction(action: ActionInterface) {
-        this.socket.emit("action", action);
+        this.socket.emit("action", {
+            data: action,
+            sender: this.me,
+        });
     }
 
-    onAction(action: ActionInterface) {
-        console.log("received action");
-        this.project.applyAction(action);
+    onAction(action: ActionNetworkPacket) {
+        console.log("Action from "+action.sender);
+        this.project.applyAction(action.data, this.selectionHandlers[action.sender]);
+    }
+
+    onHello(packet: HelloNetworkPacket) {
+        console.log("I got the selection data of "+packet.sender+": "+packet.serializedSelection);
+        this.selectionHandlers[packet.sender] = PixelSelectionHandlerFromSerialized(packet.serializedSelection);
     }
 }
