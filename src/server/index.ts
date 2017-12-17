cdbec5a041c55334fd1779e48f226b21db9e1530
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2017-12-17 23:02:20 +0100

    Server side

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,3 @@
-import {ActionInterface} from "../client/ts/tools/actionInterface";
-
-require("amd-loader");
-
 import * as express from 'express';
 import * as socketio from 'socket.io';
 import * as http from 'http';
@@ -9,6 +5,8 @@ import * as path from 'path';
 import * as serveStatic from 'serve-static';
 import {Vec2} from "../client/ts/vec2";
 import {Project} from "../client/ts/docState";
+import {ActionNetworkPacket, HelloNetworkPacket} from "../client/ts/networkLink";
+import {PixelSelectionHandler} from "../client/ts/selection/selection";
 
 let app = express();
 
@@ -26,23 +24,56 @@ app.use('/', serveStatic(path.join(__dirname, '../client/')));
 
 
 let project = new Project(null, "0", new Vec2(800,600));
+let selectionHandlers: {[id: string]: PixelSelectionHandler} = {};
+let clients: {[id: string]: SocketIO.Socket} = {};
 
 io.on("connection", function (socket: SocketIO.Socket) {
     socket.on("join", function (name: string) {
-        console.log("Client joining drawing `"+name+"`");
-
+        console.log(socket.id + " joining drawing `"+name+"`");
 
         let data = {
             dimensions: project.dimensions,
             data: project.currentLayer.getHTMLElement().toDataURL(),
         };
+
         socket.emit("joined", data);
+        for (let id in selectionHandlers) {
+            let hello: HelloNetworkPacket = {
+                sender: id,
+                serializedSelection: selectionHandlers[id].serialize(),
+            };
+            socket.emit ("hello", hello);
+        }
+
+
+        selectionHandlers[socket.id] = new PixelSelectionHandler(null, 800, 600);
+
+        let hello: HelloNetworkPacket = {
+            sender: socket.id,
+            serializedSelection: selectionHandlers[socket.id].serialize(),
+        };
+
+        for (let id in clients) {
+            clients[id].emit("hello", hello);
+        }
+
+        clients[socket.id] = socket;
     });
 
+    socket.on("disconnect", function() {
+        console.log(socket.id + " left.");
+        delete selectionHandlers[socket.id];
+        delete clients[socket.id];
+    });
 
-    socket.on("action", function (data: ActionInterface) {
+    socket.on("action", function (data: ActionNetworkPacket) {
+        if (data.sender !== socket.id) {
+          console.warn("Some guy is trying to fuck everything up.");
+          socket.disconnect();
+        }
+
         io.sockets.emit("action", data);
-        console.log("broadcasted action by tool " + data.toolName);
-        project.applyAction(data);
+        console.log("Action by " + data.sender + " with tool " + data.data.toolName);
+        project.applyAction(data.data, selectionHandlers[data.sender]);
     })
 });
