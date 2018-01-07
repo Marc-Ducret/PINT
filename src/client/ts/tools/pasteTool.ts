7bef8d67fb0e4d4641f91cdd8f94294d1532128d
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-01-07 02:50:50 +0100

    Support for composite operations in paste.

--- src/client/ts/tools/pasteTool.ts
+++ src/client/ts/tools/pasteTool.ts
@@ -9,6 +9,7 @@ import {ActionInterface, ActionType} from "./actionInterface";
  */
 export class PasteTool extends Tool {
     private ready: boolean = false;
+    readonly readahead: boolean = true;
 
     constructor() {
         super("PasteTool", "Paste", "v");
@@ -16,6 +17,34 @@ export class PasteTool extends Tool {
         this.addSetting({name: "project_clipboard", descName: "Clipboard", inputType: InputType.Hidden, defaultValue: ""});
         this.addSetting({name: "project_clipboard_x", descName: "Clipboard X", inputType: InputType.Hidden, defaultValue: 0});
         this.addSetting({name: "project_clipboard_y", descName: "Clipboard Y", inputType: InputType.Hidden, defaultValue: 0});
+        this.addSetting({name: "mode", descName: "Composition mode", inputType: InputType.Select, defaultValue: "source-over",
+            options: [
+                {name: "source-over", desc: "Source over"},
+                {name: "source-in", desc: "Source in"},
+                {name: "source-out", desc: "Source out"},
+                {name: "source-atop", desc: "Source atop"},
+                {name: "destination-over", desc: "Destination over"},
+                {name: "destination-in", desc: "Destination in"},
+                {name: "destination-out", desc: "Destination out"},
+                {name: "destination-atop", desc: "Destination atop"},
+                {name: "lighter", desc: "Lighter"},
+                {name: "xor", desc: "Xor"},
+                {name: "multiply", desc: "Multiply"},
+                {name: "screen", desc: "Screen"},
+                {name: "overlay", desc: "Overlay"},
+                {name: "darken", desc: "Darken"},
+                {name: "lighten", desc: "Lighten"},
+                {name: "color-dodge", desc: "Color dodge"},
+                {name: "color-burn", desc: "Color burn"},
+                {name: "hard-light", desc: "Hard light"},
+                {name: "soft-light", desc: "Soft light"},
+                {name: "difference", desc: "Difference"},
+                {name: "exclusion", desc: "Exclusion"},
+                {name: "hue", desc: "Hue"},
+                {name: "saturation", desc: "Saturation"},
+                {name: "color", desc: "Color"},
+                {name: "luminosity", desc: "Luminosity"}]
+        });
     }
 
     reset () {
@@ -36,15 +65,18 @@ export class PasteTool extends Tool {
     drawPreview(layer: Layer) {};
 
     async applyTool(layer: Layer, generate_undo: boolean): Promise<ActionInterface> {
-        console.log("let's work");
+        layer.getContext().save();
+        layer.getContext().globalCompositeOperation = this.getSetting("mode");
+        if (this.getSetting("mode") == "copy") {
+            layer.reset();
+        }
 
         await layer.drawDataUrl(
             this.getSetting("project_clipboard"),
             this.data.x - this.getSetting("project_clipboard_x"),
             this.data.y - this.getSetting("project_clipboard_y"));
 
-        console.log("done.");
-
+        layer.getContext().restore();
         return {
             type: ActionType.ToolApplyHistory,
             toolName: "PasteTool",
